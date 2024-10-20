ce28c8d278436514ef2a80da9e7f1de168ee1ad3
AgusDiablo <DK321081@fi365.ort.edu.uy>
2024-10-20 02:00:43 -0300

    hicimos el registrar, falta el login y la nueva password de 8 caracteres

---
+++ WebApp/Controllers/UsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using Dominio;
+using Dominio.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private Sistema _sistema = Sistema.Instancia;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+			ViewBag.Usuarios = _sistema.Usuarios;
+			return View();
+        }
+
+        public IActionResult Ver(int id)
+        {
+            return View();
+        }
+
+		[HttpGet]
+		public IActionResult IrARegistro()
+        {
+			ViewBag.Usuarios = _sistema.Usuarios;
+			// Redirige a la vista Registro
+			return View("IrARegistro");
+        }
+
+		[HttpPost]
+		public IActionResult TomarDatos(string nombre, string apellido, string mail, string contrasenia, decimal saldo)
+        {
+			ViewBag.Usuarios = _sistema.Usuarios;
+			try
+			{
+				Usuario usuario = new Cliente(saldo, nombre, apellido, mail, contrasenia);
+				_sistema.AgregarUsuario(usuario);
+				ViewBag.Nombre = nombre;
+                return View("RegistroExitoso"); // Asegúrate de tener una vista RegistroExitoso.cshtml
+			}
+			catch (Exception e)
+			{
+				ViewBag.ErrorMessage = e.Message;
+				return View("ErrorRegistro");
+			}
+		}
+
+
+    }
+}
